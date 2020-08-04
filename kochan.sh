0f87af801527036b10d59664e1399e33e83e8978
Your Name <you@example.com>
2020-08-04 13:03:08 +0300

    zxc

--- kochan.sh
+++ kochan.sh
@@ -16,6 +16,9 @@
 #'--'                   \   \  /  `--''       |  ,     .-./ '---'
 #                        `----'                `--`---'
 ############################################################
+
+1
+
 lastlog=`ls -t /var/log/nginx.log*  | grep -v gz | head -n1`
 
 cat $lastlog | awk {'print $3'}|sort|uniq >> /tmp/reestr.txt
